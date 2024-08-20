d1ffc75b3d44dd508df46b9649cb392f5fe24c8d
Ramy Marghany <ramy.med7t@gmail.com>
2024-08-20 10:38:26 +0200

    modify the prisma template and create the design page with designConfigurator component

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,8 +1,12 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { z } from "zod";
+import sharp from "sharp"
+import { db } from "@/db";
+
 const f = createUploadthing();
 
+// sever side function
 export const ourFileRouter = {
     // Define as many FileRoutes as you like, each with a unique routeSlug
     imageUploader: f({ image: { maxFileSize: "4MB" } })
@@ -13,10 +17,33 @@ export const ourFileRouter = {
             return { input };
         })
 
-        .onUploadComplete(async ({ metadata }) => {
-            // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+        .onUploadComplete(async ({ metadata, file }) => {
+            // !!! Whatever is returned here is sent to the client side `onClientUploadComplete` callback
             const { configId } = metadata.input;
-            return { configId };
+            const res = await fetch(file.url);
+            const buffer = await res.arrayBuffer();
+            const imageMetadata = await sharp(buffer).metadata();
+            const { width, height } = imageMetadata;
+            if (!configId) {
+                const configuration = await db.configuration.create({
+                    data: {
+                        imageUrl: file.url,
+                        width: width || 500,
+                        height: height || 500
+                    }
+                });
+                return { configId: configuration.id };
+            } else {
+                const updatedConfiguration = await db.configuration.update({
+                    where: {
+                        id: configId
+                    },
+                    data: {
+                        croppedImageUrl: file.url,
+                    }
+                });
+                return { configId: updatedConfiguration.id };
+            }
         }),
 } satisfies FileRouter;
 
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,35 @@
-const Page = () => {};
+import DesignConfigurator from "@/components/DesignConfigurator";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  // using async to make db call on server
+  const { id } = searchParams;
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+  if (!configuration) {
+    return notFound();
+  }
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageUrl={imageUrl}
+      imageDimensions={{ width, height }}
+    />
+  );
+};
 
 export default Page;
